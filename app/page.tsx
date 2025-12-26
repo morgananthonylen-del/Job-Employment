c9a006472aeca89f138f97a1c3a1e37603c28462
morgananthonylen-del <morgan.anthony.len@gmail.com>
2025-12-27 00:13:08 +1200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -721,7 +721,7 @@ export default function HomePage() {
           {featuredJobs.length > 0 && (
             <div className="mb-12">
               <div className="w-full px-5">
-                <div className="rounded-2xl bg-[#C55E7C] shadow-md py-[30px]">
+                <div className="rounded-2xl bg-[#C55E7C] shadow-md py-[30px] mx-[5px] md:mx-0">
                   <div className="p-5 bg-[#C55E7C] rounded-2xl grid grid-cols-1 lg:grid-cols-2 gap-6">
                     <div className="space-y-4 text-white flex flex-col justify-center items-center text-center">
                       <p className="text-[36px] leading-[44px] font-normal">
